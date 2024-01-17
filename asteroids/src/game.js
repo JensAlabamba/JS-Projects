7bc28722f3a57f5bb50dcc4f84d8a7fab255bcb3
Wojciech <klonowski.wojtek@yahoo.co.uk>
2024-01-17 13:12:40 +0100

    base game functions

--- asteroids/src/game.js
+++ asteroids/src/game.js
@@ -1,9 +1,51 @@
-function Game() {}
+const Asteroid = require("./asteoroid");
+function Game() {
+  this.asteroids = [];
 
-Game.prototype.step = function () {};
+  this.addAsteroids();
+}
 
-Game.prototype.move = function () {};
+Game.BG_COLOR = "white";
+Game.DIM_X = 1000;
+Game.DIM_Y = 600;
+Game.NUM_ASTEROIDS = 10;
 
-Game.prototype.checkCollisions = function () {};
+Game.prototype.add = function (object) {
+  if (object instanceof Asteroid) {
+    this.asteroids.push(object);
+  } else {
+    throw new Error("Invalid Object");
+  }
+};
 
-Game.prototype.draw = function (ctx) {};
+Game.prototype.addAsteroids = function () {
+  for (i = 0; i < Game.NUM_ASTEROIDS; i++) {
+    this.add(new Asteroid({ game: this }));
+  }
+};
+
+Game.prototype.randomPosition = function randomPosition() {
+  return [Game.DIM_X * Math.random(), Game.DIM_Y * Math.random()];
+};
+
+// Game.prototype.step = function () {};
+
+// Game.prototype.move = function () {};
+
+// Game.prototype.checkCollisions = function () {};
+
+Game.prototype.draw = function (ctx) {
+  ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+  ctx.fillStyle = Game.BG_COLOR;
+  ctx.fillRect(0, 0, Game.DIM_X, Game.DIM_Y);
+
+  this.asteroids.forEach(function (obj) {
+    obj.draw(ctx);
+  });
+};
+
+Game.prototype.moveObjects = function () {
+  this.asteroids.forEach(function (obj) {
+    obj.move();
+  });
+};
