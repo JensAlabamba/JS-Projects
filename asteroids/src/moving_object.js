10c9b3a0a6722230884c9f4cfe84e97af5370c2a
Wojciech <klonowski.wojtek@yahoo.co.uk>
2024-01-17 09:16:53 +0100

    setting up

---
+++ asteroids/src/moving_object.js
@@ -0,0 +1,7 @@
+function MovingObject() {}
+
+MovingObject.prototype.move = function () {};
+
+MovingObject.prototype.draw = function (ctx) {};
+
+MovingObject.prototype.isCollidedWith = function (otherMovingObject) {};
